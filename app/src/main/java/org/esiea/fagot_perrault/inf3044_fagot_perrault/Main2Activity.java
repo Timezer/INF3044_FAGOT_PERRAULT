94874a4aa4daf5ce215662170a1d8d5621821b2b
Timezer <perrault.william2@gmail.com>
2016-12-06 22:21:44 +0100

    MAJ 06/12/2016 23h22

---
+++ app/src/main/java/org/esiea/fagot_perrault/inf3044_fagot_perrault/Main2Activity.java
@@ -0,0 +1,13 @@
+package org.esiea.fagot_perrault.inf3044_fagot_perrault;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
--- app/src/main/java/org/esiea/fagot_perrault/inf3044_fagot_perrault/MainActivity.java
+++ app/src/main/java/org/esiea/fagot_perrault/inf3044_fagot_perrault/MainActivity.java
@@ -1,5 +1,8 @@
 package org.esiea.fagot_perrault.inf3044_fagot_perrault;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +15,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +48,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "N'oubliez pas de visiter notre github !", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -40,6 +61,32 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        //Date et heure
+        TextView tvDisplayDate = (TextView) findViewById(R.id.textMain_Date);
+        TextView tvDisplayHour = (TextView) findViewById(R.id.textMain_Heure);
+        TextView tvDisplaySecond = (TextView) findViewById(R.id.textMain_Second);
+
+        final Calendar c = Calendar.getInstance();
+        int yy = c.get(Calendar.YEAR);
+        int mm = c.get(Calendar.MONTH);
+        int dd = c.get(Calendar.DAY_OF_MONTH);
+        int hh = c.get(Calendar.HOUR_OF_DAY);
+        int mn = c.get(Calendar.MINUTE);
+        int ss = c.get(Calendar.SECOND);
+
+        tvDisplayDate.setText(new StringBuilder()
+                .append(dd).append("-").append(mm + 1).append("-").append(yy));
+
+        tvDisplayHour.setText(new StringBuilder()
+                .append(hh).append("h").append(mn));
+
+        tvDisplaySecond.setText(new StringBuilder()
+                .append(ss).append(" s"));
     }
 
     @Override
@@ -67,8 +114,29 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_settings_1) {
+            RelativeLayout mlayout = (RelativeLayout) findViewById(R.id.content_main);
+            mlayout.setBackgroundColor(Color.WHITE);
+        }
+        if (id == R.id.action_settings_2) {
+            RelativeLayout mlayout = (RelativeLayout) findViewById(R.id.content_main);
+            mlayout.setBackgroundColor(Color.BLUE);
+        }
+        if (id == R.id.action_settings_3) {
+            RelativeLayout mlayout = (RelativeLayout) findViewById(R.id.content_main);
+            mlayout.setBackgroundColor(Color.GRAY);
+        }
+        if (id == R.id.action_settings_4) {
+            RelativeLayout mlayout = (RelativeLayout) findViewById(R.id.content_main);
+            mlayout.setBackgroundColor(Color.GREEN);
+        }
+        if (id == R.id.action_settings_5) {
+            RelativeLayout mlayout = (RelativeLayout) findViewById(R.id.content_main);
+            mlayout.setBackgroundColor(Color.MAGENTA);
+        }
+        if (id == R.id.action_settings_6) {
+            RelativeLayout mlayout = (RelativeLayout) findViewById(R.id.content_main);
+            mlayout.setBackgroundColor(Color.YELLOW);
         }
 
         return super.onOptionsItemSelected(item);
@@ -80,17 +148,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_a_propos) {
+            Intent appel = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(appel);
+        } else if (id == R.id.nav_json) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_toast) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_youtube) {
 
         }
 
@@ -98,4 +163,30 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void refresh(View v) {
+        TextView tvDisplayDate = (TextView) findViewById(R.id.textMain_Date);
+        TextView tvDisplayHour = (TextView) findViewById(R.id.textMain_Heure);
+        TextView tvDisplaySecond = (TextView) findViewById(R.id.textMain_Second);
+
+        final Calendar c = Calendar.getInstance();
+        int yy = c.get(Calendar.YEAR);
+        int mm = c.get(Calendar.MONTH);
+        int dd = c.get(Calendar.DAY_OF_MONTH);
+        int hh = c.get(Calendar.HOUR_OF_DAY);
+        int mn = c.get(Calendar.MINUTE);
+        int ss = c.get(Calendar.SECOND);
+
+        tvDisplayDate.setText(new StringBuilder()
+                .append(dd).append("-").append(mm + 1).append("-").append(yy));
+
+        tvDisplayHour.setText(new StringBuilder()
+                .append(hh).append("h").append(mn));
+
+        tvDisplaySecond.setText(new StringBuilder()
+                .append(ss).append(" s"));
+
+        Toast.makeText(getApplicationContext(),getString(R.string.msg_refresh), Toast.LENGTH_LONG).show();
+    }
+
 }
