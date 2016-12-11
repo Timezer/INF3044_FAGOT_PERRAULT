1a34d9bff86d2469fb0bc2ba77ed6838083367a5
Timezer <perrault.william2@gmail.com>
2016-12-11 21:19:43 +0100

    MAJ 11/12/2016 21h19

--- app/src/main/java/org/esiea/fagot_perrault/inf3044_fagot_perrault/Main5Activity.java
+++ app/src/main/java/org/esiea/fagot_perrault/inf3044_fagot_perrault/Main5Activity.java
@@ -1,14 +1,19 @@
 package org.esiea.fagot_perrault.inf3044_fagot_perrault;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -16,6 +21,10 @@ public class Main5Activity extends AppCompatActivity {
 
     public DatePickerDialog dpd = null;
     public TimePickerDialog tpd = null;
+    public String testdate = "null";
+    public String testheure = "null";
+    public String msg = "Sans message";
+    public static final int DIALOG_ALERT = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +51,7 @@ public class Main5Activity extends AppCompatActivity {
         TimePickerDialog.OnTimeSetListener otsl = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timepicker, int hour, int minute) {
-                choose2.setText(hour+":"+minute);
+                choose2.setText(hour+"H"+minute);
             }
         };
         tpd = new TimePickerDialog(this, otsl, hh, mn, true);
@@ -50,10 +59,58 @@ public class Main5Activity extends AppCompatActivity {
 
     public void choose(View v) {
         dpd.show();
+        testdate = "ok";
     }
 
     public void choose2(View v) {
         tpd.show();
+        testheure = "ok";
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        final EditText et = (EditText) findViewById(R.id.calendar_edittext);
+        final Button date = (Button) findViewById(R.id.choose);
+        final Button heure = (Button) findViewById(R.id.choose2);
+        switch (id) {
+            case DIALOG_ALERT:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Voulez-vous ajouter l'évenement\n"
+                        + "\"" + et.getText() + "\"" + " à votre calendrier\n" +
+                        "pour le " + date.getText() + " à " + heure.getText() + " ?");
+                builder.setCancelable(true);
+                builder.setPositiveButton("Valider", new OkOnClickListener());
+                builder.setNegativeButton("Annuler", new CancelOnClickListener());
+                AlertDialog dialog = builder.create();
+                dialog.show();
+        }
+        return super.onCreateDialog(id);
+    }
+
+    private final class CancelOnClickListener implements
+            DialogInterface.OnClickListener {
+        public void onClick(DialogInterface dialog, int which) {
+            Toast.makeText(getApplicationContext(), "Enregistrement annulé", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private final class OkOnClickListener implements
+            DialogInterface.OnClickListener {
+        public void onClick(DialogInterface dialog, int which) {
+            Main5Activity.this.finish();
+        }
+    }
+
+    public void validate(View v) {
+        if (testdate == "ok" && testheure == "ok") {
+            showDialog(DIALOG_ALERT);
+        }
+        else if (testdate == "null") {
+            Toast.makeText(getApplicationContext(), "Veuillez choisir une date", Toast.LENGTH_SHORT).show();
+        }
+        else if (testheure == "null") {
+            Toast.makeText(getApplicationContext(), "Veuillez choisir une heure", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
